c5fcca5cb41f1d02caccbcc22e88db429cf114b6
cuppatetleys <51878036+cuppatetleys@users.noreply.github.com>
2019-09-03 20:58:46 +0100

    Update and rename RoomTransfer.cs to RoomMove.cs

--- RoomMove.cs
+++ RoomMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class RoomMove : MonoBehaviour
@@ -8,6 +9,12 @@ public class RoomMove : MonoBehaviour
     public float cameraChangeY;
     public Vector3 PlayerChange;
     private CameraMovement cam;
+
+    //for Title card
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
 
     private void Start()
     {
@@ -25,6 +32,19 @@ public class RoomMove : MonoBehaviour
             cam.maxposition.y += cameraChangeY;
 
             collision.transform.position += PlayerChange;
+
+            if(needText)
+            {
+                StartCoroutine(ShowPlaceName());
+            }
         }
+    }
+
+    private IEnumerator ShowPlaceName()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
     }
 }
