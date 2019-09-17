270ad017bfb0ea4ff0179f789e25d8505a07b17a
cuppatetleys <51878036+cuppatetleys@users.noreply.github.com>
2019-09-17 17:59:24 +0100

    Update RoomMove.cs

--- RoomMove.cs
+++ RoomMove.cs
@@ -23,7 +23,7 @@ public class RoomMove : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if(collision.CompareTag("Player") && !collision.isTrigger)
         {
             cam.minposition.x += cameraChangeX;
             cam.minposition.y += cameraChangeY;
